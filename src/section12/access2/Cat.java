8a99c93094fa0adcadda8766cf388b9176e83747
1emonac <jay.kang2@gmail.com>
2023-08-07 22:29:16 +0900

    1

---
+++ src/section12/access2/Cat.java
@@ -0,0 +1,15 @@
+package section12.access2;
+
+public interface Cat {
+	
+	public static final int PAW = 4;	// 상수 (final 생략가능) 무조건 상수
+	
+	
+	public void eat();
+	
+	public void sleep();
+	
+	public void hunt();
+	
+
+}
---
+++ src/section13/OuterClass02.java
@@ -0,0 +1,32 @@
+package section13;
+/*
+ * 내부클래스 컴파일
+ * 
+ * 	Outer$Inner.class
+ * 
+ * 	내부클래스도 외부클래스 안에 생성되는 것 외에도 별도의 클래스이기에 
+ * 	컴파일시 별도로 생성된다.
+ * 
+ */
+public class OuterClass02 {
+	public static void main(String[] args) {
+		
+		new OuterClass02.InnerClass().info();
+		OuterClass02.InnerClass inner = new OuterClass02.InnerClass();
+		inner.info();
+		
+		OuterClass02.InnerClass.staticInfo();
+		
+	}
+	
+	static class InnerClass {
+		public static void staticInfo() {
+			System.out.println("static 메서드 입니다.");
+		}
+		
+		public void info() {
+			System.out.println("static 내부클래스의 일반메서드 입니다.");
+		}
+	}
+
+}
